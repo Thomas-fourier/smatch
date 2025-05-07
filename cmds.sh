fe21eee505eb2750df3767fd66c35cd31ed14224
Thomas Fourier <fourier.thomas@gmail.com>
2025-05-07 20:51:28 +0200

    clean output and update script

--- cmds.sh
+++ cmds.sh
@@ -4,8 +4,8 @@
 SMATCH_DIR=dirname "$(realpath $0)"
 
 
-env CHECK_VARS=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_final_apis
-$SMATCH_DIR/parse_apis.pl /log_final_apis > $SMATCH_DIR/kernel_apis.h
+env CHECK_VARS=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_apis
+$SMATCH_DIR/parse_apis.pl /log_apis > $SMATCH_DIR/kernel_apis.h
 make -C $SMATCH_DIR/ clean
 make -C $SMATCH_DIR/ -j `nproc`
-env CHECK_DEREF=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_final_deref
+env CHECK_DEREF=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_api_deref
