e7f08e1c9dd8b7625695a9202baa1a6f13286c50
Thomas Fourier <fourier.thomas@gmail.com>
2025-05-12 13:55:52 +0200

    use state to track argument id and function

--- check_apis_tag_args.c
+++ check_apis_tag_args.c
@@ -5,10 +5,6 @@
 #include <assert.h>
 
 
-// Only track if a variable is of interest, and print 
-STATE(api_arg);
-
-
 //#define debug(...) fprintf(stderr, __VA_ARGS__)
 #define debug(...)
 
@@ -20,12 +16,20 @@ static int in_interesting_file = 0;
 static int in_interesting_function = 0;
 static struct kernel_api_func *current_api_func = NULL;
 static GHashTable *previously_found_funcs = NULL;
+static GHashTable *arg_states = NULL;
 
 /* How is a variable used? */
 struct usage_context {
 	int atomic;
 	char *atomic_expr;
 };
+
+/* The id of an API argument */
+struct api_arg {
+   struct kernel_api_func *api_func;
+   int arg_id;
+};
+
 
 static int is_same_driver(const char *api_file, const char *file) {
 	// Do the files belong to the same directory?
@@ -55,11 +59,21 @@ static void build_function_list(void) {
     }
  }
 
+static unsigned int pointer_hash(const void *ptr) {
+   return (long)ptr;
+}
+
+static int pointer_eq(const void *ptr1, const void *ptr2) {
+   return ptr1 == ptr2;
+}
 
 static void match_fundef(struct symbol *sym)
 {
    if(previously_found_funcs == NULL)
       build_function_list();
+
+   if(arg_states == NULL)
+      arg_states = g_hash_table_new(&pointer_hash, &pointer_eq);
 
    struct kernel_api_func *k = g_hash_table_lookup(previously_found_funcs, get_function());
    if(k) {
@@ -71,15 +85,30 @@ static void match_fundef(struct symbol *sym)
       printf("API function %s (%s.%s) basefile %s\n", get_function(), current_api_func->api_name, current_api_func->api_field, get_base_file());
       {
          struct symbol *arg;
+         int i = 0;
 
          FOR_EACH_PTR(sym->ctype.base_type->arguments, arg) {
             if (!arg->ident) {
                continue;
             }
-            set_state(my_id, arg->ident->name, arg, &api_arg);
+
+            // Two states are saved: one to check that the variable is of interest, another to
+            // save the index of the argument
+            struct smatch_state *arg_state = malloc(sizeof(*arg_state));
+            arg_state->name = NULL;
+            
+            struct api_arg *aa = malloc(sizeof(*aa));
+            aa->api_func = current_api_func;
+            aa->arg_id = i;
+
+            g_hash_table_insert(arg_states, arg_state, aa);
+
+            set_state(my_id, arg->ident->name, arg, arg_state);
+
+            i++;
          } END_FOR_EACH_PTR(arg);
       }
-   } 
+   }
    // callchain_add_fun(get_filename(), get_function(), my_id);
 }
 
@@ -95,20 +124,38 @@ static void match_func_end(struct symbol *sym)
    // callchain_rm_fun(get_function(), my_id);
 }
 
-
-static bool is_api_arg(struct expression *expr) {
+static struct api_arg *get_arg_from_tag(struct expression *expr) {
 
 	struct sm_state *sm;
 
 	sm = get_sm_state_expr(my_id, expr);
-	if (sm && slist_has_state(sm->possible, &api_arg))
-		return 1;
-	return 0;
+	if (!sm) {
+      // fprintf(stderr, "Trying to get arg of possible non-arg\n");
+      return NULL;
+   }
+
+   struct sm_state *tmp;
+   struct api_arg *res;
+
+	FOR_EACH_PTR(sm->possible, tmp) {
+
+      res = g_hash_table_lookup(arg_states, tmp->state);
+      if (res != 0)
+         return res;
+	} END_FOR_EACH_PTR(tmp);
+	return NULL;
 }
 
 static void match_deref(struct expression *expr) {
-   if (is_api_arg(expr)) {
-      printf("deref of API in function %s, API name %s, field %s\n", get_function(), current_api_func->api_name, current_api_func->api_field);
+
+   struct api_arg *arg = get_arg_from_tag(expr);
+   if (arg) {
+      printf("deref of (%s.%s.%d) %s:%d\n", 
+            arg->api_func->api_func,
+            arg->api_func->api_field,
+            arg->arg_id,
+            get_filename(),
+            get_lineno());
    }
 }
 
