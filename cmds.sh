2c8d8190d902f51758d862a7c475d30a17e69c6d
Thomas Fourier <fourier.thomas@gmail.com>
2025-05-14 15:27:34 +0200

    update cmds to merge log files

--- cmds.sh
+++ cmds.sh
@@ -3,9 +3,14 @@
 
 SMATCH_DIR=$(dirname "$(realpath $0)")
 
-
-env CHECK_APIS=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_apis
-$SMATCH_DIR/parse_apis.pl /log_apis > $SMATCH_DIR/kernel_apis.h
+# To prevent races when writing to the output, each thread has its own file
+# and outputs are fusioned together afterwards. 
+env CHECK_APIS=TRUE OUTFILE=log_api $SMATCH_DIR/smatch_scripts/test_kernel.sh
+cat log_api/* > log_api_cat
+$SMATCH_DIR/parse_apis.pl /log_apis_cat > $SMATCH_DIR/kernel_apis.h
 make -C $SMATCH_DIR/ clean
 make -C $SMATCH_DIR/ -j `nproc`
-env CHECK_DEREF=TRUE $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_api_deref
+env CHECK_DEREF=TRUE OUTFILE=log_api_args $SMATCH_DIR/smatch_scripts/test_kernel.sh | tee log_api_deref
+cat log_api_args/* > log_api_args_cat
+$SMATCH_DIR/parse_api_args.pl log_api_cat
+
