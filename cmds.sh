73d8930cbe743a0f25a0e8392f7d78a4b64e55da
Thomas Fourier <fourier.thomas@gmail.com>
2025-05-15 15:16:55 +0200

    fix typos

--- cmds.sh
+++ cmds.sh
@@ -5,12 +5,12 @@ SMATCH_DIR=$(dirname "$(realpath $0)")
 
 # To prevent races when writing to the output, each thread has its own file
 # and outputs are fusioned together afterwards. 
-env CHECK_APIS=TRUE OUTFILE=log_api $SMATCH_DIR/smatch_scripts/test_kernel.sh
-cat log_api/* > log_api_cat
-$SMATCH_DIR/parse_apis.pl log_api_cat > $SMATCH_DIR/kernel_api.inc.h
+env CHECK_APIS=TRUE OUTFILE=log_apis $SMATCH_DIR/smatch_scripts/test_kernel.sh
+cat log_apis/* > log_apis_cat
+$SMATCH_DIR/parse_apis.pl log_apis_cat > $SMATCH_DIR/kernel_apis.inc.h
 make -C $SMATCH_DIR/ clean
 make -C $SMATCH_DIR/ -j `nproc`
-env CHECK_DEREF=TRUE OUTFILE=log_api_args $SMATCH_DIR/smatch_scripts/test_kernel.sh
-cat log_api_args/* > log_api_args_cat
-$SMATCH_DIR/parse_api_args.pl log_api_cat
+env CHECK_DEREF=TRUE OUTFILE=log_apis_args $SMATCH_DIR/smatch_scripts/test_kernel.sh
+cat log_apis_args/* > log_apis_args_cat
+$SMATCH_DIR/parse_apis_args.pl log_apis_cat
 
