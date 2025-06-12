6c26146e6706089cf572d3fbd588b5c489f60e17
Thomas Fourier <fourier.thomas@gmail.com>
2025-06-12 10:44:31 +0200

    refactor `check_dma`

--- check_dma.c
+++ check_dma.c
@@ -1,6 +1,7 @@
 
 #include "smatch.h"
 #include "smatch_extra.h"
+#include "smatch_slist.h"
 #include "string.h"
 
 static int my_id;
@@ -11,10 +12,19 @@ STATE(tested_dma);
 int untested_dma_count = 0;
 
 // Sometimes, the state tracking is wrong, for instance in a[i]
-// If two expressions are the same, we assume it is ok.
+// If two expressions are the same as strings, assume, they are
+// actually the same.
 char *last_dma_map = NULL;
 
-static void match_dma_map(struct expression *expr) {
+static const char *dma_mapping_functions[] = {
+    "dma_map_single",
+    "dma_map_single_attrs",
+    "dma_map_page",
+    "dma_map_page_attrs",
+    "dma_map_area",
+};
+
+static void match_dma_map(const char *fn, struct expression *expr, void *unused) {
     struct expression *parent = expr_get_parent_expr(expr);
 
     set_state_expr(my_id, expr, &untested_dma);
@@ -27,64 +37,27 @@ static void match_dma_map(struct expression *expr) {
     if (last_dma_map)
         free_string(last_dma_map);
     last_dma_map = expr_to_str(parent->left);
-    
+
     untested_dma_count++;
 }
 
-static void match_dma_error(struct expression *expr) {
+static void match_dma_error(const char *fn, struct expression *expr, void *unused) {
     struct expression *arg = get_argument_from_call_expr(expr->args, 1);
 
-    //printf("dma_mapping_test %s\n", expr_to_str(expr));
+    if (get_state_expr(my_id, arg) == &tested_dma) {
+        sm_warning("dma_mapping_error called on an already tested dma pointer.");
+    }
 
-    if (!expr_has_possible_state(my_id, arg, &untested_dma)) {
-        if (last_dma_map && strcmp(last_dma_map, expr_to_str(arg)) == 0) {
-            // If the last dma_map was the same as this one, it is ok
-            goto ok;
-        }
+    if (!(expr_has_possible_state(my_id, arg, &untested_dma) ||
+        (last_dma_map && strcmp(last_dma_map, expr_to_str(arg)) == 0))) {
         sm_warning("dma_mapping_error called with %s which is not dma'd", expr_to_str(arg));
         return;
     }
 
-ok:
     set_state_expr(my_id, arg, &tested_dma);
     free_string(last_dma_map);
     last_dma_map = NULL;
     untested_dma_count--;
-}
-
-static void match_call(struct expression *expr) {
-    if (expr->type != EXPR_CALL || !expr->fn)
-        return;
-
-    if (!get_function())
-        return;
-
-    // If it is the implementation of dma_map or dma_mapping_error, ignore
-    if (strstr(get_function(), "dma_map") ||
-        strstr(get_function(), "dma_mapping_error")) {
-        return;
-    }
-
-    char *fn_name = expr_to_str(expr->fn);
-    if (!fn_name)
-        return;
-
-    if (strstr(fn_name, "dma_map_sg") ||
-        strstr(fn_name, "rdma"))
-        goto free;
-
-    if (strstr(fn_name, "dma_mapping_error")) {
-        match_dma_error(expr);
-        goto free;
-    }
-
-    if (strstr(fn_name, "dma_map")) {
-        match_dma_map(expr);
-        goto free;
-    }
-
- free:
-    free_string(fn_name);
 }
 
 
@@ -94,10 +67,27 @@ static void match_func_end(struct symbol *sym) {
         return;
     }
 
-    if (untested_dma_count && !strstr(get_function(), "dma_map")) {
+    struct stree *stree;
+	struct sm_state *tmp;
+    bool found_untested = false;
+
+	stree = __get_cur_stree();
+	FOR_EACH_MY_SM(my_id, stree, tmp) {
+		if (slist_has_state(tmp->possible, &untested_dma)) {
+            sm_warning("possible dma mapping not tested of pointer %s", tmp->name);
+            found_untested = true;
+        }
+	} END_FOR_EACH_SM(tmp);
+
+
+    if (!found_untested && untested_dma_count && !strstr(get_function(), "dma_map")) {
         sm_warning("Function %s has %d untested dma_map calls", get_function(), untested_dma_count);
     }
     untested_dma_count = 0;
+
+    free_string(last_dma_map);
+    last_dma_map = NULL;
+
 }
 
 
@@ -108,9 +98,10 @@ void check_dma(int id)
         return;
     }
 
-    // TODO: ignore map_sg
-    // TODO: ignore rdma
+    add_function_hook("dma_mapping_error", match_dma_error, NULL);
 
-    add_hook(match_call, FUNCTION_CALL_HOOK);
+    for (int i = 0; i < ARRAY_SIZE(dma_mapping_functions); i++)
+        add_function_hook(dma_mapping_functions[i], match_dma_map, NULL);
+
     add_hook(match_func_end, END_FUNC_HOOK);
 }
