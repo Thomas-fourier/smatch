7e821dda28c9c11449bf683f33d426faa1fe7850
Thomas Fourier <fourier.thomas@gmail.com>
2025-08-14 14:21:41 +0200

    Properly stringify calls

--- check_generic_args.c
+++ check_generic_args.c
@@ -119,6 +119,8 @@ static bool is_cast(struct expression *expr) {
     }
 }
 
+static char *stringify_call(struct expression *expr);
+
 static char *stringify(struct expression *expr) {
     char *res;
     char *pp;
@@ -137,6 +139,10 @@ static char *stringify(struct expression *expr) {
     if (expr->type == EXPR_PREOP && (expr->op == '&' || expr->op == '*'))
         return stringify(expr->unop);
 
+    if (expr->type == EXPR_CALL) {
+        return stringify_call(expr);
+    }
+
     if (expr->type == EXPR_DEREF && expr->member) {
         asprintf(&res, "(%s)->%s", type_to_str(get_type(expr->deref)),
                  expr->member->name);
@@ -151,6 +157,45 @@ static char *stringify(struct expression *expr) {
 
     return res;
 }
+
+
+static char *stringify_call(struct expression *expr)
+{
+    int nb_args = ptr_list_size((struct ptr_list *)expr->args);
+    char **str_args;
+    int len = 0;
+    char *res;
+    char *p;
+    int i;
+
+    char *func = stringify(expr->fn);
+    len = strlen(func) + 1;
+    if (!nb_args) {
+        res = realloc(func, len + 2);
+        strcpy(res + len - 1, "()");
+        return res;
+    }
+
+    str_args = malloc(nb_args * sizeof(*str_args));
+    for (i = 0; i < nb_args; i++) {
+        str_args[i] = stringify(get_argument_from_call_expr(expr->args,
+                                                                  i));
+        len += (2 + strlen(str_args[i]));
+    }
+
+    res = malloc(len);
+    p = res;
+    p += sprintf(p, "%s(", func);
+    free_string(func);
+    for (i = 0; i < nb_args; i++) {
+        p += sprintf(p, i == nb_args - 1 ? "%s)" : "%s, ", str_args[i]);
+        free_string(str_args[i]);
+    }
+    free(str_args);
+
+    return res;
+}
+
 
 /* Tests if a string is in a list. If it is, write index with the index in the
 list if found */
