1d40cfcc5a9595c88344d4ed587d41b554925d46
Thomas Fourier <fourier.thomas@gmail.com>
2025-11-07 14:06:11 +0100

    check if function pairs mention each other in docstring

--- parse_func_pair.pl
+++ parse_func_pair.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl
 
+
+use File::Basename;
+use File::Map qw(map_file);
+
+my $filename = $ARGV[0];
 my %score = ();
 my %func_def = ();
 
@@ -9,6 +14,35 @@ sub avg {
         $res += $i;
     }
     return $res / @_
+}
+
+sub is_in_doc {
+    my ($fun1, $fun2) = @_;
+    # path of header (suppose logs is in linux folder)
+    if ($func_def{$fun1} eq "") {
+        print("No definition of the function $fun1 found\n");
+        return 0;
+    }
+    my $file_fun1 = dirname($filename) . "/" . $func_def{$fun1};
+    map_file my $file_cat, $file_fun1;
+    # Match /* ... */ ... (EOF|/|{|}|;)
+    my @matches = ($file_cat =~ m/(\/\*(?:\*[^\/]|[^*])*\*\/[^\Z\/{}\;]*)/g);
+
+    for my $i (@matches) {
+        # There is exactly one end of comment, check that
+        my $end_match = substr($i, index($i, "*/"));
+        if (-1 != index($end_match, $fun1)) {
+            $i = substr($i, 0, index($i, "*/"));
+            return index($i, $fun2) != -1;
+        }
+    }
+    print("No documentation found for $fun1");
+    return 0;
+}
+
+sub linked_by_doc {
+    my ($fun1, $fun2) = @_;
+    return is_in_doc($fun1, $fun2) || is_in_doc($fun2, $fun2);
 }
 
 while(<>) {
@@ -29,8 +63,11 @@ my @res;
 for my $fun1 (keys %score) {
     #print $fun1 . "\n";
     for my $fun2 (keys %{$score{$fun1}}) {
-        if ($func_def{$fun1} eq $func_def{$fun2}) {
-            push(@res, scalar(@{$score{$fun1}{$fun2}}) # sqrt(avg(@{$score{$fun1}{$fun2}})) / distance($fun1, $fun2)
+        if ($func_def{$fun1} eq $func_def{$fun2}
+            && scalar(@{$score{$fun1}{$fun2}}) > 5
+            && linked_by_doc($fun1, $fun2)) {
+            push(@res, avg(@{$score{$fun1}{$fun2}})
+                 . " " . @{$score{$fun1}{$fun2}}
                  . " " . $fun1 . " " . $fun2 . " " . $func_def{$fun1});
         }
     }
